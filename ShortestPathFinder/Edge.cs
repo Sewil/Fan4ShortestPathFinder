ec793bc39b5e5f23708e16a0373b4a0af65d9537
pandaranta <rillokka@hotmail.com>
2016-01-13 20:32:36 +0100

    Added:
    Edge.cs
    Node.cs
    Path.cs
    PathTraveler.cs

---
+++ ShortestPathFinder/Edge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeTransportationLimited.Graphs
+{
+    class Edge
+    {
+
+        public Edge(Node node1, Node node2)
+        {
+            Node1 = node1;
+            Node2 = node2;
+        }
+
+        public Node Node1 { get; set; }
+        public Node Node2 { get; set; }
+
+    }
+}
---
+++ ShortestPathFinder/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeTransportationLimited.Graphs
+{
+    class Node
+    {
+        public Node[] Neighbours { get; set; }        
+    }
+}
