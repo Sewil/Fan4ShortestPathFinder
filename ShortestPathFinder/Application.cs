6549c347aac148c8f9a35556ed7f24045942c160
Jemsann <jemma.touma@gmail.com>
2016-01-14 00:26:35 +0100

    Editerat runmetoden för att hantera input och output

--- ShortestPathFinder/Application.cs
+++ ShortestPathFinder/Application.cs
@@ -11,50 +11,84 @@
 // och anropa sedan de klassernas metoder från metoden Application.Run() som
 // finns i denna fil.
 
+
+
 namespace NodeTransportationLimited.Graphs.ShortestPathFinder
-{    
-/// <summary>
-/// Denna klass startar Shortest Path Finder-programmet. <b>VARNING: Inga
-/// ändringar får göras till denna klass förrutom där det anges att det är
-/// tillåtet i dokumentationen.</b>
-/// </summary>
+{
+	/// <summary>
+	/// Denna klass startar Shortest Path Finder-programmet. <b>VARNING: Inga
+	/// ändringar får göras till denna klass förrutom där det anges att det är
+	/// tillåtet i dokumentationen.</b>
+	/// </summary>
 	public static class Application
 	{
-        /// <summary>
-        /// Startar exekveringen av Shortest Path Finder-programmet. Metoden anropas
-        /// från Application.Main() för att starta programmet, men den anropas även
-        /// vid testning. Då projektet överlämnas till Lernia Consulting AB så ger
-        /// metoden korrekt utdata för endast två testfall. Detta är enbart för
-        /// ett demonstrationssyfte och ska tas bort.<br/><br/>
-        /// 
-        /// <b>VARNING: Ändringar får göras i metodens <i>kropp</i>
-        /// (eng. <i>body</i>) men i övrigt får metoden inte ändras.</b>
-        /// </summary>
-        public static void Run()
-        {
-            // Ni får göra ändringar här inuti denna metods kropp (eng. body).
+		/// <summary>
+		/// Startar exekveringen av Shortest Path Finder-programmet. Metoden anropas
+		/// från Application.Main() för att starta programmet, men den anropas även
+		/// vid testning. Då projektet överlämnas till Lernia Consulting AB så ger
+		/// metoden korrekt utdata för endast två testfall. Detta är enbart för
+		/// ett demonstrationssyfte och ska tas bort.<br/><br/>
+		/// 
+		/// <b>VARNING: Ändringar får göras i metodens <i>kropp</i>
+		/// (eng. <i>body</i>) men i övrigt får metoden inte ändras.</b>
+		/// </summary>
+		public static void Run()
+		{
 
-            System.Console.WriteLine("Provide graph data now.");
+			var ConnectionsList = new System.Collections.Generic.List<Connection>(262144);
 
-            string nrNodesStr = System.Console.ReadLine();
-            string edgesStr = System.Console.ReadLine();
-            string startEndStr = System.Console.ReadLine();
-            string output = null;
-           
+			System.Console.WriteLine("Provide graph data now.");
 
-            System.Console.WriteLine(output);
+			string nrOfNodes = System.Console.ReadLine();
+			string edges = System.Console.ReadLine();//"0 1, 1 2, 2 3"
+			string findPath = System.Console.ReadLine();//"0 4"
 
-            // Ni får *inte* göra några fler ändringar efter denna linje utanför denna
-            // metods kropp.
-        }
+			int nodes = int.Parse(nrOfNodes);
 
-        /// <summary>
-        /// Anropas när programmet startas.<br/><br/>
-        /// 
-        /// <b>VARNING: Denna metod får inte ändrans på något sätt.</b>
-        /// </summary>
-        /// <exclude/>
-        public static void Main()
+			Node[] nodesArray = new Node[nodes];
+			for (int i = 0; i < nodesArray.Length; i++)
+			{
+				nodesArray[i] = new Node(i);
+			}
+
+			string[] connectionArray = System.Text.RegularExpressions.Regex.Split(edges, ", ");//"0 1"
+			foreach (var connection in connectionArray)
+			{
+				string[] nodeArray = System.Text.RegularExpressions.Regex.Split(connection, " ");
+
+				ConnectionsList.Add(new Connection(nodesArray[int.Parse(nodeArray[0])], nodesArray[int.Parse(nodeArray[1])]));
+			}
+
+			string[] firstEndNodes = System.Text.RegularExpressions.Regex.Split(findPath, " ");
+			int firstNode = int.Parse(firstEndNodes[0]);
+			int endNode = int.Parse(firstEndNodes[1]);
+
+			Path shortestPath = PathTraveler.Run_Algorithm(nodesArray[firstNode], nodesArray[endNode]);
+			string output = null;
+			for (int i = 0; i < shortestPath.listOfPath.Count; i++)
+			{
+				var connection = shortestPath.listOfPath[i];
+				output += connection.Node1.Id.ToString();
+				if(i == shortestPath.listOfPath.Count-1)
+				{
+					break;
+				}
+				output += ", ";
+			}
+			
+			System.Console.WriteLine(output);
+
+			// Ni får *inte* göra några fler ändringar efter denna linje utanför denna
+			// metods kropp.
+		}
+
+		/// <summary>
+		/// Anropas när programmet startas.<br/><br/>
+		/// 
+		/// <b>VARNING: Denna metod får inte ändrans på något sätt.</b>
+		/// </summary>
+		/// <exclude/>
+		public static void Main()
 		{
 			Run();
 		}
